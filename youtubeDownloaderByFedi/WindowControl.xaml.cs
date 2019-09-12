46df2808aee3100ac5083b82f3e56159ac47cefe
fedi <fedi.dayeg@hotmail.com>
2019-09-13 00:56:51 +0100

    Ajoutez des fichiers projet.

---
+++ youtubeDownloaderByFedi/WindowControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace youtubeDownloaderByFedi
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowControl.xaml
+    /// </summary>
+    public partial class WindowControl : UserControl
+    {
+        public WindowControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
